bb689cd6d84dabc9cd3f59bd4514ec297863b178
YoulingZXY <YoulingZXY@github.com>
2018-02-26 19:34:19 +0800

    测试Eclipse提交

--- src/main/java/com/zxy/test3/web/CustomerController.java
+++ src/main/java/com/zxy/test3/web/CustomerController.java
@@ -21,7 +21,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
 	
-	@RequestMapping
+	@RequestMapping("hello")
 	public String hello() {
 		return "hello";
 	}
